a65de6e0b4070105f1ca0744f4f1e52bbba88865
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-11-08 17:13:54 -0800

    fix updated date function

--- src/SkyLab/Foundation/helpers.php
+++ src/SkyLab/Foundation/helpers.php
@@ -73,9 +73,9 @@ if ( ! function_exists( 'launchpad_entry_meta' ) ) {
         if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
             $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 
-            if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-                $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-            }
+            // if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+            //     $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+            // }
 
             $time_string = sprintf( $time_string,
                 esc_attr( get_the_date( 'c' ) ),
